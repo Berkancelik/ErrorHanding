3dd13086dd5b75e6b5acbef3ce6035eab21209c5
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-14 15:06:34 +0300

    Add project files.

---
+++ ErrorHanding/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using ErrorHanding.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorHanding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //throw new Exception("Veri tabanına bağlanırken bir hata meydana geldi");
+            int value1 = 5;
+            int value2 = 0;
+            int result = value1 / value2;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ErrorHanding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ErrorHanding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
